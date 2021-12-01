e5dce9f59115bf33343e87a0e2aa951bb6989b4f
Hugo <blackfoxmmm@gmail.com>
2021-12-01 15:28:40 +0500

    scss

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,32 @@ const { src, dest } = require("gulp"),
   include = require("gulp-file-include"),
   htmlmin = require("gulp-htmlmin"),
   del = require("del"),
+  concat = require("gulp-concat"),
+  autoprefixer = require("gulp-autoprefixer"),
   sync = require("browser-sync").create();
+
+function html() {
+  return src("src/**.html")
+    .pipe(
+      include({
+        prefix: "@@",
+      })
+    )
+    .pipe(dest("dist"));
+}
+
+function scss() {
+  return src("src/scss/**.scss")
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+      })
+    )
+    .pipe(csso())
+    .pipe(concat("index.css"))
+    .pipe(dest("dist"));
+}
+
+exports.html = html;
+exports.scss = scss;
